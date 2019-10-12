28934df27e25c32f228228260888520796cbe1b9
15872998154 <1978912861@qq.com>
2019-10-12 16:31:09 +0800

    index page and config admin

--- chilly_blog/blog/views.py
+++ chilly_blog/blog/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import  Blog, BlogType
 # Create your views here.
 def index(request):
-    return HttpResponse("index")
+    context = {}
+    blog_types = BlogType.objects.all()
+    context['blog_types'] = blog_types
+    context['latest_blogs'] = Blog.objects.all().order_by('-update_time')[:5]
+    context['hot_blogs'] = Blog.objects.all().order_by('-read_count','-update_time')[:5]
+    return render(request, 'blog/index.html', context)
