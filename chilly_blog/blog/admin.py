846e64d8b527eea00d833823371f02675ca7bba6
15872998154 <1978912861@qq.com>
2019-10-12 17:23:43 +0800

    classufy blog with date

--- chilly_blog/blog/admin.py
+++ chilly_blog/blog/admin.py
@@ -3,8 +3,10 @@ from .models import  Blog,BlogType
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
+    list_filter = ('blog_type', 'update_time')
     list_display = ('id', 'title', 'blog_type', 'update_time', 'read_count') #显示字段
     list_display_links = ('id', 'title', 'blog_type', 'update_time', 'read_count') #点击可进入编辑界面
+    # list_editable = ['update_time'] #可以编辑的字段
 
     def blog_type(self, obj):
         return obj.blog_type.type_name
--- chilly_blog/blog/views.py
+++ chilly_blog/blog/views.py
@@ -8,4 +8,6 @@ def index(request):
     context['blog_types'] = blog_types
     context['latest_blogs'] = Blog.objects.all().order_by('-update_time')[:5]
     context['hot_blogs'] = Blog.objects.all().order_by('-read_count','-update_time')[:5]
+    blog_dates = Blog.objects.dates('update_time', 'month', order='DESC')
+    print(blog_dates)
     return render(request, 'blog/index.html', context)
